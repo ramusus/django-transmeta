6f78e111047c98a3d9a4924573d6f9af20df848a
Fabio Corneti <info@corneti.com>
2009-07-09 16:23:47 +0200

    Setuptools integration

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = '0.1svn'
+
+setup(
+    name='django-transmeta',
+    version=version,
+    description="Generic tagging application for Django",
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = ['Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Internationalization',
+        ],
+    keywords='django i18n translation',
+    author='Marc Garcia',
+    author_email='garcia.marc@gmail.com',
+    url='http://code.google.com/p/django-transmeta/',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+)
